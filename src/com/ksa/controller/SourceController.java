04aa7c743585fb5830df769dc2d5b539020eda0e
jradams97 <justinadams7991@gmail.com>
2019-10-23 21:20:03 +1100

    feat(source): Adjustment to SourceController
    - add Database entity to global model (for easy access)
    - add example Javadoc to SourceController for reference

--- src/com/ksa/controller/SourceController.java
+++ src/com/ksa/controller/SourceController.java
@@ -2,13 +2,14 @@ package com.ksa.controller;
 
 import com.ksa.XMLDataLoader;
 import com.ksa.model.MainModel;
-import com.ksa.model.SourceModel;
 import com.ksa.model.entity.Database;
 import com.ksa.view.SourceView;
+import com.ksa.model.SourceModel;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
+import javafx.stage.Window;
 
 import javax.xml.bind.JAXBException;
 import java.io.File;
@@ -17,25 +18,93 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.stream.Stream;
 
+/**
+ * This controller handles events and user actions that execute on the 'Source'
+ * page of the application. It contains a reference to the application
+ * {@link Stage} and a {@code dataLoader} instance for parsing XML file
+ * content. References the MVC pattern by aggregating an associated
+ * {@code view} ({@link SourceView}) and {@code model} ({@link SourceModel})
+ *
+ * @author jradams97
+ * @author tianlu102238612
+ * @version 0.2
+ */
 public class SourceController implements EventHandler {
 
+	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	//  Fields
+	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+	/**
+	 * Reference to application stage.
+	 */
 	private final Stage mainStage;
+
+	/**
+	 * Associated view. Provided a controller instance on initialisation.
+	 */
 	private final SourceView view = new SourceView(this);
+
+	/**
+	 * Associated model. Provided a controller instance on initialisation.
+	 */
+	private final SourceModel model = new SourceModel(this);
+
+	/**
+	 * Data handling class used to oversee parsing of XML information and
+	 * validate entity structure.
+	 */
 	private static final XMLDataLoader dataLoader = new XMLDataLoader();
-	private static File sourceFile;
+
+	/**
+	 * File obtained through {@link FileChooser#showOpenDialog(Window)}
+	 * action. Shared through different event triggers
+	 */
+	private File sourceFile;
+
+	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	//  Accessors and Mutators
+	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 	public Stage getMainStage() {
 		return mainStage;
 	}
 
-	public SourceView getView() {
+	SourceView getView() {
 		return view;
 	}
 
-	public SourceController(final Stage mainStage) {
+	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	//  Constructor(s)
+	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+	/**
+	 * Class constructor. References the main stage of the application
+	 *
+	 * @param mainStage  Global stage used for all application classes.
+	 */
+	SourceController(final Stage mainStage) {
 		this.mainStage = mainStage;
 	}
 
+	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	//  Methods
+	//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+	/**
+	 * Overridden implementation of {@link EventHandler#handle(Event)} that
+	 * determines actions to perform when user actions are performed.
+	 * {@code browseBtn} is handled to prompt for an input file. The selected
+	 * source file is then parsed as a Java entity (POJO) and stored in a
+	 * global model ({@link MainModel}) for easy access on other scenes.
+	 * {@code loadBtn} is handled to take the obtained {@code sourceFile},
+	 * stream its contents into the global model, and display it in the
+	 * text area ({@code xmlSourceFld})
+	 *
+	 * @param event  Obtained from the {@link javafx.event.EventDispatcher}.
+	 *               Defines a "notification" when a specific action occurs.
+	 * @see Stream
+	 */
 	@Override
 	public void handle(final Event event) {
 		final Object source = event.getSource();
@@ -47,15 +116,16 @@ public class SourceController implements EventHandler {
 
 			Database movieDatabase = new Database();
 
-			XMLDataLoader dataLoader = new XMLDataLoader();
 			try {
 				movieDatabase = dataLoader.parseAsMovie(sourceFile);
 			} catch (JAXBException e) {
+				// TODO: Add logger
 				System.out.println("JAXB error. " + e);
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
 
-			System.out.println(movieDatabase.getClass().toString());
-			String test = "test";
+			MainModel.setDatabaseEntity(movieDatabase);
 
 		} else if (source.equals(view.getLoadBtn())) {
 			StringBuilder builder = new StringBuilder();
@@ -66,6 +136,8 @@ public class SourceController implements EventHandler {
 				} catch (IOException ioEx) {
 					// TODO: Add logger
 					ioEx.printStackTrace();
+				} catch (Exception e) {
+					e.printStackTrace();
 				}
 
 				MainModel.setDatabaseContent(builder.toString());
--- src/com/ksa/model/MainModel.java
+++ src/com/ksa/model/MainModel.java
@@ -1,8 +1,11 @@
 package com.ksa.model;
 
+import com.ksa.model.entity.Database;
+
 public class MainModel {
 
 	private static String databaseContent;
+	private static Database databaseEntity;
 
 	public static String getDatabaseContent() {
 		return databaseContent;
@@ -10,5 +13,13 @@ public class MainModel {
 
 	public static void setDatabaseContent(String databaseContent) {
 		MainModel.databaseContent = databaseContent;
+	}
+
+	public static Database getDatabaseEntity() {
+		return databaseEntity;
+	}
+
+	public static void setDatabaseEntity(Database databaseEntity) {
+		MainModel.databaseEntity = databaseEntity;
 	}
 }
