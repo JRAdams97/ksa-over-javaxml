3d1b0ff314d5c309f2158918b2b727d1dd3c8405
田露 <tianlu@tianludeMacBook-Pro.local>
2019-10-26 10:39:54 +1100

    add search controller changes

--- src/com/ksa/controller/VisualizationController.java
+++ src/com/ksa/controller/VisualizationController.java
@@ -1,9 +1,16 @@
 package com.ksa.controller;
 
+import java.util.Hashtable;
+import java.util.Set;
+
+import com.ksa.model.MainModel;
 import com.ksa.view.VisualizationView;
+
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.stage.Stage;
 
-public class VisualizationController {
+public class VisualizationController implements EventHandler {
 
 	private final Stage stage;
 	private final VisualizationView view = new VisualizationView(this);
@@ -19,4 +26,30 @@ public class VisualizationController {
 	public VisualizationController(Stage primaryStage) {
 		this.stage = primaryStage;
 	}
+	
+	@Override
+	public void handle(final Event event) {
+		final Object source = event.getSource();
+		Hashtable visualTable = MainModel.getCorrelatedKeywords();
+		System.out.println(visualTable);
+		Set<String> keys = visualTable.keySet();
+		
+		if(source.equals(view.getTop3Keywords()))
+				{
+					for(String key: keys) {
+						System.out.println(key+"--"+visualTable.get(key));
+			    }
+			
+				}
+	    
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
 }
--- src/com/ksa/model/MainModel.java
+++ src/com/ksa/model/MainModel.java
@@ -1,11 +1,14 @@
 package com.ksa.model;
 
-import com.ksa.model.entity.Database;
+import java.util.Hashtable;
 
 public class MainModel {
 
 	private static String databaseContent;
-	private static Database databaseEntity;
+	private static String titleContent;
+	private static String movieContent;
+	private static Hashtable correlatedKeywords;
+	
 
 	public static String getDatabaseContent() {
 		return databaseContent;
@@ -14,12 +17,22 @@ public class MainModel {
 	public static void setDatabaseContent(String databaseContent) {
 		MainModel.databaseContent = databaseContent;
 	}
-
-	public static Database getDatabaseEntity() {
-		return databaseEntity;
+	
+	public static String getTitleContent() {
+		return titleContent;
 	}
 
-	public static void setDatabaseEntity(Database databaseEntity) {
-		MainModel.databaseEntity = databaseEntity;
+	public static void setTitleContent(String titleContent) {
+		MainModel.titleContent = titleContent;
 	}
+	
+	public static Hashtable getCorrelatedKeywords() {
+		return correlatedKeywords;
+	}
+
+	public static void setCorrelatedKeywords(Hashtable correlatedKeywords) {
+		MainModel.correlatedKeywords = correlatedKeywords;
+	}
+	
+	
 }
